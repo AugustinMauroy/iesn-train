4d7e93f375969518464c0235de5d96699366effe
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-11-17 11:40:01 +0100

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
       },
     }),
     tailwindcss(),  
+    VitePWA({ registerType: 'autoUpdate' })
   ],
 })
